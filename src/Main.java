cf9428f2f33954e983b828867f55ca6bea01a4b0
Christian <Christianmnaira@gmail.com>
2025-02-09 21:00:15 +0800

    feat: added a method for prompting user to enter user info

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+
+
+    }
+
+    public void askUser() {
+
+        Scanner askUser = new Scanner(System.in);
+        String first_name;
+        String last_name;
+        String middle_name;
+
+        System.out.print("Enter your First Name: ");
+        first_name = askUser.nextLine();
+
+        System.out.print("Enter your Last Name: ");
+        last_name = askUser.nextLine();
+
+        System.out.print("Enter your Middle Name: ");
+        middle_name = askUser.nextLine();
 
 
     }
