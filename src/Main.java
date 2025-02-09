60fcf6925791b1b3e778014d96caf5ede963525d
Christian <Christianmnaira@gmail.com>
2025-02-09 21:15:32 +0800

    feat: used User class to store user String inputs

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Main userObj = new Main();
-        userObj.askUser();
+        Main userObj = new Main(); // create an instance or object for the Main class
+        userObj.askUser(); // Call askUser method to execute its function
 
     }
 
     public void askUser() {
 
+        // three empty variables to store user input after prompting
         String first_name;
         String last_name;
         String middle_name;
 
-        Scanner askUser = new Scanner(System.in);
+        User addUser = new User(); // create an instance or object for the User class
+
+        Scanner askUser = new Scanner(System.in); // create an instance or object for the Scanner class
 
         System.out.print("Enter your First Name: ");
         first_name = askUser.nextLine();
@@ -26,7 +29,10 @@ public class Main {
         System.out.print("Enter your Middle Name: ");
         middle_name = askUser.nextLine();
 
-        System.out.println(first_name + middle_name + last_name);
+        addUser.setFirst_name(first_name);
+        addUser.setLast_name(last_name);
+        addUser.setMiddle_name(middle_name);
+
 
     }
 
