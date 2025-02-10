9a1de78ad755f50f6f734f03d9fef19cd0eb2063
Christian <Christianmnaira@gmail.com>
2025-02-10 09:12:09 +0800

    fix: correctly arranged the proper of getting the User class by using an inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,26 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main extends User {
 
     public static void main(String[] args) {
 
-        Main userObj = new Main(); // create an instance or object for the Main class
-        userObj.askUser(); // Call askUser method to execute its function
+        Main user = new Main();
+
+        user.setFirst_name("Christian");
+        user.setLast_name("Naira");
+        user.setMiddle_name("Mercado");
+
+        System.out.println(user.getFirst_name());
+        System.out.println(user.getLast_name());
+        System.out.println(user.getMiddle_name());
+
+        printOut(user);
 
     }
 
-    public void askUser() {
+    static void printOut(Main user){
 
-        // three empty variables to store user input after prompting
-        String first_name;
-        String last_name;
-        String middle_name;
-
-        User addUser = new User(); // create an instance or object for the User class
-
-        Scanner askUser = new Scanner(System.in); // create an instance or object for the Scanner class
-
-        System.out.print("Enter your First Name: ");
-        first_name = askUser.nextLine();
-
-        System.out.print("Enter your Last Name: ");
-        last_name = askUser.nextLine();
-
-        System.out.print("Enter your Middle Name: ");
-        middle_name = askUser.nextLine();
-
-        addUser.setFirst_name(first_name);
-        addUser.setLast_name(last_name);
-        addUser.setMiddle_name(middle_name);
+        System.out.println(user.getFirst_name());
 
 
     }
